bd0cbd840ca2db2be5020aee4e2af2fb6c2e4728
eustacedyke <eustacedyke@gmail.com>
2023-06-29 22:54:56 +0100

    newsletter

--- src/Home/subscribe.jsx
+++ src/Home/subscribe.jsx
@@ -1,12 +1,16 @@
 function Subscribe() {
     return (
       <div className="Subscribe">
-        <h2>SUBSRIBE TO OUR LETTER</h2>
+        
+        
+        <div>
+        <h2>SUBSRIBE TO OUR NEWSLETTER</h2> <br />
 
         <form action="">
             <input type="email" name="" id="" placeholder="Enter you email here" />
             <button>OK</button>
         </form>
+        </div>
         
       </div>
     );
