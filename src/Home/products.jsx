aab4adec585f743c218b7665c7e6069faa39e4a9
Eustace <98954452+eustacedike@users.noreply.github.com>
2023-08-25 07:12:57 +0000

    hardone

--- src/Home/products.jsx
+++ src/Home/products.jsx
@@ -1,80 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-
-import product1 from '../Images/product1.jpg';
-import product2 from '../Images/product2.jpg';
-import product3 from '../Images/product3.jpg';
-import product4 from '../Images/product4.jpg';
-import product5 from '../Images/product5.jpg';
-import product6 from '../Images/product6.jpg';
-import product7 from '../Images/product7.jpg';
-import product8 from '../Images/product8.jpg';
-import product9 from '../Images/product9.jpg';
-import product10 from '../Images/product10.jpg';
-import product11 from '../Images/product11.jpg';
-import product12 from '../Images/product12.jpg';
-import product13 from '../Images/product13.jpg';
-
-
-
-
-
+import product1 from "../Images/product1.jpg";
+import product2 from "../Images/product2.jpg";
+import product3 from "../Images/product3.jpg";
+import product4 from "../Images/product4.jpg";
+import product5 from "../Images/product5.jpg";
+import product6 from "../Images/product6.jpg";
+import product7 from "../Images/product7.jpg";
+import product8 from "../Images/product8.jpg";
+import product9 from "../Images/product9.jpg";
+import product10 from "../Images/product10.jpg";
+import product11 from "../Images/product11.jpg";
+import product12 from "../Images/product12.jpg";
+import product13 from "../Images/product13.jpg";
 
 function Products() {
+  const galleryPics = [
+    { image: product1, legend: "Fair Package" },
+    { image: product2, legend: "Fair Package" },
+    { image: product3, legend: "Fair Package" },
+    { image: product4, legend: "Fair Package" },
+    { image: product5, legend: "Fair Package" },
+    { image: product6, legend: "Fair Package" },
+    { image: product7, legend: "Fair Package" },
+    { image: product8, legend: "Fair Package" },
+    { image: product9, legend: "Fair Package" },
+    { image: product10, legend: "Fair Package" },
+    { image: product11, legend: "Fair Package" },
+    { image: product12, legend: "Fair Package" },
+    { image: product13, legend: "Fair Package" },
+  ];
 
+  const currentThumb = useRef([]);
+  const thumbss = useRef();
 
+  useEffect(() => {
+    currentThumb.current = currentThumb.current.slice(0, galleryPics.length);
+  }, [galleryPics]);
 
+  useEffect(() => {
+    console.log(document.documentElement.clientWidth);
+    console.log(thumbss.current.getBoundingClientRect());
+    console.log(galleryPics[12].image);
+  }, []);
 
-
-    const galleryPics = [
-        { image: product1, legend: "Fair Package" },
-        { image: product2, legend: "Fair Package" },
-        { image: product3, legend: "Fair Package" },
-        { image: product4, legend: "Fair Package" },
-        { image: product5, legend: "Fair Package" },
-        { image: product6, legend: "Fair Package" },
-        { image: product7, legend: "Fair Package" },
-        { image: product8, legend: "Fair Package" },
-        { image: product9, legend: "Fair Package" },
-        { image: product10, legend: "Fair Package" },
-        { image: product11, legend: "Fair Package" },
-        { image: product12, legend: "Fair Package" },
-        { image: product13, legend: "Fair Package" },
-        
-    ];
-
-    const [inView, setInView] = useState(1);
-
-    const next = () => {
-        if (galleryPics.length == inView + 1){
-            setInView(0)
-        } else {
-            setInView(inView + 1)
-        }
+  const scrolltoThumbIfNotVisible = (d) => {
+    const bounded = currentThumb.current[d].getBoundingClientRect();
+    console.log(bounded.left);
+    if (
+      !(
+        bounded.top >= 0 &&
+        bounded.left >= 0 &&
+        bounded.bottom <= document.documentElement.clientHeight &&
+        bounded.right <= document.documentElement.clientWidth
+      )
+    ) {
+      thumbss.current.scrollTo(bounded.left, 0);
     }
-    
-    const previous = () => {
-        if (inView == 0){
-            setInView(galleryPics.length - 1)
-        } else {
-            setInView(inView - 1)
-        }
+  };
+
+  const [inView, setInView] = useState(1);
+
+  const next = () => {
+    if (galleryPics.length == inView + 1) {
+      setInView(1);
+      scrolltoThumbIfNotVisible(0);
+    } else {
+      setInView(inView + 1);
+      scrolltoThumbIfNotVisible(inView - 1);
+      console.log(inView);
     }
+    console.log(inView);
+  };
 
+  const previous = () => {
+    if (inView == 1) {
+      setInView(galleryPics.length);
+      scrolltoThumbIfNotVisible(galleryPics.length - 1);
+    } else {
+      setInView(inView - 1);
+      scrolltoThumbIfNotVisible(inView - 1);
+    }
+    scrolltoThumbIfNotVisible(inView);
+  };
 
-    return (
-        <div className="Gallery">
-
-<div className="gallery-1">
-
-            {/* <Carousel id="maxi">
+  return (
+    <div className="Gallery">
+      <div className="gallery-1">
+        {/* <Carousel id="maxi">
 
                 {
                     galleryPics.map(photo => {
@@ -90,46 +110,68 @@ function Products() {
 
             </Carousel> */}
 
-            <div className="">
-                {/* <h2 className='close-album' onClick={() => setPhotoDisplay(-1)}>&#10006;</h2> */}
+        <div className="">
+          {/* <h2 className='close-album' onClick={() => setPhotoDisplay(-1)}>&#10006;</h2> */}
 
-                {
-  
-                            <img src={galleryPics[inView].image} id="mini"/>
+          {<img src={galleryPics[inView - 1].image} id="mini" />}
+          <p className="capt"> {galleryPics[inView - 1].legend} </p>
 
-                }
+          <div className="thumbnails" ref={thumbss}>
+            {galleryPics.map((thumb, i) => {
+              return (
+                <div id="">
+                  <img
+                    ref={(el) => (currentThumb.current[i] = el)}
+                    src={thumb.image}
+                    className="thumb"
+                    style={{
+                      border:
+                        galleryPics.indexOf(thumb) + 1 == inView
+                          ? "4px solid orange"
+                          : "3px solid white",
+                    }}
+                    onClick={() => {
+                      setInView(galleryPics.indexOf(thumb) + 1);
+                    }}
+                  />
+                  {/* <button onClick={scrolltoThumbIfNotVisible}>tdfdx</button> */}
+                  {/* <p>
+                    {bounded.top >= 0 &&
+                      bounded.left >= 0 &&
+                      bounded.bottom <=
+                        (window.innerHeight ||
+                          document.documentElement.clientHeight) &&
+                      bounded.right <=
+                        (window.innerWidth ||
+                          document.documentElement.clientWidth)}
+                  </p> */}
 
-                <div className="thumbnails">
-                    {galleryPics.map(thumb => {
-                        return (
-                            <div id=''>
-                                <img
-                                src={thumb.image}
-                                className='thumb'
-                                style={{border: galleryPics.indexOf(thumb)+1 == inView? "4px solid orange" : "4px solid white"}}
-                                onClick={()=>{setInView(galleryPics.indexOf(thumb)+1)}}
-                                />
-                                {/* <p>{galleryPics.indexOf(thumb)}</p> */}
-                            </div>
-                        )
-                    })}
+                  {/* <p>{galleryPics.indexOf(thumb)}</p> */}
                 </div>
+              );
+            })}
+          </div>
 
-                <div className="prd-nxt" onClick={next}><FaChevronRight/></div>
-                <div className="prd-prv" onClick={previous}><FaChevronLeft/></div>
-                {/* <div className="position"><b>{photoDisplay + 1}</b></div> */}
-
-            </div>
-
-            
-</div>
-<div className="gallery-2">
-    <h1>OUR PACKAGES</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut architecto est laudantium aliquid ut ipsum quam obcaecati illo blanditiis beatae.</p>
-    {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, nesciunt magni? Quibusdam ex maxime quam iste accusamus laudantium odit? Explicabo!</p> */}
-</div>
+          <div className="prd-nxt" onClick={next}>
+            <FaChevronRight />
+          </div>
+          <div className="prd-prv" onClick={previous}>
+            <FaChevronLeft />
+          </div>
+          {/* <div className="position"><b>{photoDisplay + 1}</b></div> */}
         </div>
-    );
+      </div>
+      <div className="gallery-2">
+        <h1>OUR PACKAGES</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+          architecto est laudantium aliquid ut ipsum quam obcaecati illo
+          blanditiis beatae.
+        </p>
+        {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, nesciunt magni? Quibusdam ex maxime quam iste accusamus laudantium odit? Explicabo!</p> */}
+      </div>
+    </div>
+  );
 }
 export default Products;
 
